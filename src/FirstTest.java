9167a39317dc1f3b9108c14ac1e1a0df299f9b54
VikaGavrika <gavriviktoriia@gmail.com>
2024-03-03 00:42:44 +0300

    Тест Отмены поиска. Метод дожидаемся эдемента строка поиска и кликаем по нему. Метод дожидаемся кнопки возврата и кликаем по ней, Метод, который проверяет,что после нажатия "Назад", мы вернулись на страницу, где нет элемента стрелки "назад"

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -35,7 +35,8 @@ public class FirstTest {
         driver.quit();
     }
     @Test
-    public void firstTest() throws InterruptedException {
+    public void firstTest() throws InterruptedException
+    {
         driver.findElementByXPath("//*[@text='Skip']").click();
 
         //модернизировали метод поиска элемента и клика
@@ -60,8 +61,36 @@ public class FirstTest {
                 15
         );
 
+    }
+
+    @Test
+    public void testCanselSearch()
+    {
+        //Смахиваем онбординг
+        driver.findElementByXPath("//*[@text='Skip']").click();
+        //дожидаемся эдемента строка поиска и кликаем по нему
+        waitForElementByIdAndClick(
+                "org.wikipedia:id/search_container",
+                "Cannot find search input",
+                5
+        );
+
+        //дожидаемся кнопки возврата и кликаем по ней
+        waitForElementByXpathAndClick(
+                "//*[@content-desc='Navigate up']",
+                "Cannot find back-button to cancel search",
+                5
+        );
+        //Метод, который проверяет,что после нажатия "Назад", мы вернулись на страницу, где нет элемента стрелки "назад"
+        waitForElementNotPresent(
+                "//*[@content-desc='Navigate up']",
+                "Cannot find back-button to cancel search",
+                5
+        );
 
     }
+
+
     //метод, который ищет элемент по xpath с задержкой. В течение этих сек ищет пока не найдет,если не найдет то сообщение об ошибке. Задержку в сек прописываем в тесте
     private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeoutInSecond)
     {
@@ -93,6 +122,38 @@ public class FirstTest {
         element.sendKeys(value);
         return element;
     }
+
+    //метод поиска элемента по ID
+    private WebElement waitForElementPresentById(String id, String error_message, long timeoutInSecond)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSecond);
+        wait.withMessage(error_message +"\n");
+        By by = By.id(id);
+        return wait.until(
+                //ждем выполнения конкретного условия, ждем элемент by
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+    private WebElement waitForElementByIdAndClick(String id, String error_message, long timeoutInSecond)
+    {
+        WebElement element = waitForElementPresentById(id,error_message,timeoutInSecond);
+        element.click();
+        return element;
+    }
+
+    //Метод отсутствия элемента на странице
+    private boolean waitForElementNotPresent(String xpath, String error_message, long timeoutInSecond)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSecond);
+        wait.withMessage(error_message +"\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                //ждем выполнения конкретного условия, ждем элемент by
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+
+    }
+
 
 }
 
