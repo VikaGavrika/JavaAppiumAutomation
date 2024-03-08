6aee434bf6e7710f882332316e2979d10b1bbaa8
VikaGavrika <gavriviktoriia@gmail.com>
2024-03-08 22:28:37 +0300

    Создан отдельный класс CoreTestCase, куда перенесли методы запуска и закрытия. Сделано наследование.

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,43 +1,14 @@
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
-import org.junit.Before;
+import lib.CoreTestCase;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.URL;
+public class FirstTest extends CoreTestCase {
 
-import static org.junit.Assert.assertEquals;
-
-public class FirstTest {
-    private AppiumDriver driver;
-    @Before
-    public void setUp () throws Exception
-    {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("appium:platformName","Android");
-        capabilities.setCapability("appium:deviceName","AndroidTestDevice");
-        capabilities.setCapability("appium:platformVersion","14.0");
-        capabilities.setCapability("appium:automationName","UiAutomator2");
-        capabilities.setCapability("appium:appPackage","org.wikipedia");
-        capabilities.setCapability("appium:appActivity",".main.MainActivity");
-        capabilities.setCapability("appium:app","C:\\Users\\veekeetee\\Desktop\\JavaAppiumAutomation\\apks\\wikipedia2023-12-12.apk");
-
-
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), capabilities);
-
-    }
-    @After
-    public void tearDown()
-    {
-        driver.quit();
-    }
     @Test
-    public void firstTest() throws InterruptedException
+    public void testSearch() throws InterruptedException
     {
         driver.findElementByXPath("//*[@text='Skip']").click();
 
@@ -163,6 +134,8 @@ public class FirstTest {
         assertElementHasText(inputElement, expectedText);
 
     }
+    //Тест, который делает поиск по какому-то слову. Затем убеждается, найдены несколько статей со словом в листе результатов,
+    // затем удаляет результаты поиска и убеждается что лист с результатами пуст
     @Test
     public void testSearchAndCanselSearch()
     {
@@ -210,7 +183,7 @@ public class FirstTest {
 
     //тест, который делает поиск по какому-то слову. Затем убеждается, что в каждом результате поиска есть это слово.
     @Test
-    public void SearchTextAndCheckTextInTitles()
+    public void testSearchTextAndCheckTextInTitles()
     {
         //Смахиваем онбординг
         driver.findElementByXPath("//*[@text='Skip']").click();
---
+++ src/lib/CoreTestCase.java
@@ -0,0 +1,40 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class CoreTestCase extends TestCase {
+    //аппиум драйвер будет использоваться в и других классах поэтому протектед
+    protected AppiumDriver driver;
+    //выносим урл в отдельную переменную
+    private static String AppiumUrl = "http://127.0.0.1:4723";
+
+    @Override
+    public void setUp () throws Exception
+    {
+        super.setUp();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("appium:platformName","Android");
+        capabilities.setCapability("appium:deviceName","AndroidTestDevice");
+        capabilities.setCapability("appium:platformVersion","14.0");
+        capabilities.setCapability("appium:automationName","UiAutomator2");
+        capabilities.setCapability("appium:appPackage","org.wikipedia");
+        capabilities.setCapability("appium:appActivity",".main.MainActivity");
+        capabilities.setCapability("appium:app","C:\\Users\\veekeetee\\Desktop\\JavaAppiumAutomation\\apks\\wikipedia2023-12-12.apk");
+
+
+        driver = new AndroidDriver(new URL(AppiumUrl), capabilities);
+
+    }
+    @Override
+    public void tearDown() throws Exception
+    {
+        driver.quit();
+        super.tearDown();
+    }
+}
